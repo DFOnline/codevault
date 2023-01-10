3f6bd0aa8877b0fb2ada41906164f1ad8726cb13
GeorgeRNG <grogrng@gmail.com>
2023-01-10 16:53:04 +0000

    I'm not sure where I'm going tbh

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
+var sqlite3_1 = __importDefault(require("sqlite3"));
+var sqlite_1 = require("sqlite");
+var PORT = 8000;
+var DB = (0, sqlite_1.open)({ driver: sqlite3_1["default"], filename: '/dist/data.db' });
 var APP = (0, express_1["default"])();
 APP.get('/', function (_req, res) {
     res.send('ping');
 });
-APP.listen(8000);
+APP.listen(PORT, function () {
+    console.log("App listening at port ".concat(PORT));
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import Express from 'express';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
 
-const PORT = 8000
+const PORT = 8000;
 
+const DB  = open({driver: sqlite3.Database, filename: '/dist/data.db'});
 const APP = Express();
 
 APP.get('/', (_req, res) => {
