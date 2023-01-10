f9a658445b7313c365756a26e62cd68680b3a307
GeorgeRNG <grogrng@gmail.com>
2023-01-10 16:34:42 +0000

    Add an express app

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var express_1 = __importDefault(require("express"));
+var APP = (0, express_1["default"])();
+APP.get('/', function (_req, res) {
+    res.send('ping');
+});
+APP.listen(8000);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+import Express from 'express';
+
+const PORT = 8000
+
+const APP = Express();
+
+APP.get('/', (_req, res) => {
+    res.send('ping');
+})
+
+APP.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`);
+});
