fd30f5688fc519fb18d3e78f9c615098011b2115
GeorgeRNG <grogrng@gmail.com>
2023-01-10 18:41:19 +0000

    someone else can do the sqlite

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,39 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 exports.__esModule = true;
-var express_1 = __importDefault(require("express"));
-var sqlite3_1 = __importDefault(require("sqlite3"));
-var sqlite_1 = require("sqlite");
+var express_1 = __importStar(require("express"));
 var PORT = 8000;
-var DB = (0, sqlite_1.open)({ driver: sqlite3_1["default"], filename: '/dist/data.db' });
+// const DB  = open({driver: sqlite3.Database, filename: '/dist/data.db'});
 var APP = (0, express_1["default"])();
 APP.get('/', function (_req, res) {
     res.send('ping');
+});
+APP.post('/upload', (0, express_1.json)(), function (req, res) {
+    console.log(req.headers['user-agent']);
+    console.log(req.body);
+    res.send();
 });
 APP.listen(PORT, function () {
     console.log("App listening at port ".concat(PORT));
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import Express from 'express';
+import Express, { json } from 'express';
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
 const PORT = 8000;
 
-const DB  = open({driver: sqlite3.Database, filename: '/dist/data.db'});
+// const DB  = open({driver: sqlite3.Database, filename: '/dist/data.db'});
 const APP = Express();
 
 APP.get('/', (_req, res) => {
     res.send('ping');
+});
+
+APP.post('/upload', json(), (req, res) => {
+    console.log(req.headers['user-agent']);
+    console.log(req.body);
+    res.send();
 })
 
 APP.listen(PORT, () => {
